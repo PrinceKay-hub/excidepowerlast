8ef2ffaf91415b66688d2322b3b907a05da58f42
PrinceKay-hub <koduahprince67@gmail.com>
2026-05-15 18:33:10 +0000

    Commit API server app.ts update

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,10 +1,11 @@
-import express, { type Express, type Request, type Response } from "express";
-import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
+import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
+import * as pinoHttpNamespace from 'pino-http';
+import router from './routes';
+import { logger } from './lib/logger';
 
 const app: Express = express();
+const pinoHttp = pinoHttpNamespace.default; 
 
 app.use(
   pinoHttp({
@@ -14,7 +15,7 @@ app.use(
         return {
           id: req.id,
           method: req.method,
-          url: req.url?.split("?")[0],
+          url: req.url?.split('?')[0],
         };
       },
       res(res: Response) {
@@ -25,10 +26,10 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", router);
+app.use('/api', router);
 
 export default app;
